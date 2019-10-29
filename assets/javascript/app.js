cf99353415e114bf3773c90c68543f2ac44c7f37
Monica Amitin <monzielvsu@gmail.com>
2019-10-28 18:24:33 -0700

    creaded folders and files

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//Initial array of basketball topics
+
+//Call gifURL
+
+//Performing an AJAX request with the queryURL
+//After data comes back from request
+
+//Storing the data from the AJAX request in the results
+//Looping through each result time
+//Creating and storing a div tag
+//Creating a paragraph tag with the results item's rating
+//Creating and storing an image tag
+//Setting the src attribute of the image to a property pulled off the result item's rating
