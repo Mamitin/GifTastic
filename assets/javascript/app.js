70768467cb8fbdea27013e73de86f1e45308a929
Amitin <MAmitin@academyart.edu>
2019-10-29 12:23:37 -0700

    finished psuedocoding app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,10 @@
-//Initial array of basketball topics
+//Initial array of athletes
 
-//Call gifURL
+//Create function re-renders the HTML to display the appropriate content
 
 //Performing an AJAX request with the queryURL
-//After data comes back from request
+//Creating a div to hold the athlete
+
 
 //Storing the data from the AJAX request in the results
 //Looping through each result time
@@ -11,3 +12,24 @@
 //Creating a paragraph tag with the results item's rating
 //Creating and storing an image tag
 //Setting the src attribute of the image to a property pulled off the result item's rating
+//Appending the image
+
+//Function for displaying athletes data
+//Deleting the previous gifs
+
+//Loop through array of topics 
+
+//Dynamically gernerate buttons for each basketball athletes
+//Adding a class to the buttns
+//Adding data-attribute
+//Add button to the div
+
+
+//Create function for when button is clicked
+//Grab input from textbox
+//Add input from textbox to array
+
+
+//Add event listener to all elements with class
+
+//Calling function to diplay initial buttons
