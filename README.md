ecb1dfb728d9be15291cfc761f5dff5464d64377
Monica Amitin <monzielvsu@gmail.com>
2019-11-02 18:04:20 -0700

    updated readme, fixed error on cpnsole by fixing bootstrap link

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# GifTastic
+# UC Berkeley Fullstack Coding Bootcamp
+
+# Giftastic
+
+## Overveiw:
+In this assignment, I used the GIPHY API to make a dynamic web page that populates with gifs of popular athletes. I had to call the GIPHY API and use JavaScript and jQuery to change the HTML of my site.
+
+## How you play the game?
+- When the user clicks on a button, the page grabs 10 static, non-animated gif images from the GIPHY API and place them on the page.
+- When the user clicks one of the still GIPHY images, the gif will animate.
+- When the user clicks the gif again, it should stop playing.
+- Under every gif, displayed is its rating (PG, G). This data is provided by the GIPHY API.
+- In the search area the user can type an athlete and hitthe submit button.
+- It will make a new button with the new athletes name.
+- Once the user clicks on the new button it will generate 10 new gifs of that athlete.
+
+## Game in action: 
+<img src="/assets/images/gifTastic.gif" width="500px" height="250px"/>
+
+## Important code snippit: 
+This particaular code generates the timer for the game.
+
+<img src="assets/images/snippit.PNG">
+
+
+## Currently working with:
+
+- Html
+- CSS
+- JavaScript
+- jQuery
+- GitBash
+- GitLab
+- GitHub
+- Timers
+- API
+
+## Assignments (works in progress):
+
+- [Profile](https://github.com/Mamitin/Basic-portfolio/blob/master/portfolio.html)
+- [Portfolio](https://github.com/Mamitin/Basic-portfolio/blob/master/portfolio.html)
+- [Contact](https://github.com/Mamitin/Basic-portfolio/blob/master/contact.html)
+- [Wireframe](https://github.com/Mamitin/HW-Wireframe/blob/master/index.html)
+- [Bootstrap Portfolio](https://github.com/Mamitin/Bootstrap-Portfolio/blob/master/index.html)
+- [Crytal Collector](https://mamitin.github.io/unit-4-game/blob/master/index.html)
+- [Simpson Trivia Game](https://github.com/Mamitin/TriviaGame)
+- [GifTastic]()
+
+## Contributing
+Please find me on [Linkedin](https://www.linkedin.com/in/monica-amitin-58635475/).
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,100 +1,106 @@
+$(document).ready(function () {
+
+    //Initial array of famous athletes
+    var topics = ["Michael Jordon", "Tiger Woods", "Marshawn Lynch", "Kobe Bryant", "Klay Thompson", "Eli Manning", "Lebron James", "Blake Griffin", "David Beckham", "Reggie Bush"];
 
 
-//Initial array of famous athletes
-var topics = ["Michael Jordon", "Tiger Woods", "Marshawn Lynch", "Kobe Bryant", "Klay Thompson", "Eli Manning", "Lebron James", "Blake Griffith", "David Beckham", "Reggie Bush"];
+    //Create function re-renders the HTML to display the appropriate content
+    function displayAthletesGif() {
+        var athletes = $(this).attr("data-athletes");
 
+        //Create queryURL
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + athletes + "&api_key=rqcp4alraOM85IAywNsBaaG7PoyVJBV2&limit=10&offset=0&rating=PG&lang=en"
 
-//Create function re-renders the HTML to display the appropriate content
-function displayAthletesGif() {
-    var athletes = $(this).attr("data-athletes");
-    
-    //Create queryURL
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + athletes + "&api_key=rqcp4alraOM85IAywNsBaaG7PoyVJBV2&limit=10&offset=0&rating=PG&lang=en"
+        //Performing an AJAX request with the queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
 
-    //Performing an AJAX request with the queryURL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
+            var results = response.data;
 
-        var results = response.data;
+            //Clear previous gifs
+            $(".gifDiv").empty();
 
-        //Clear previous gifs
-        $(".gifDiv").empty();
+            //Looping through each 
+            for (var i = 0; i < results.length; i++) {
+                //Creating and storing a div tag
+                var gifDiv = $("<div>").attr("class", "gifDiv float-md-left");
+                //console.log();
+                //Creating and storing an image tag
+                var gifImage = $("<img>");
+                //Setting the attribute of the image
+                gifImage.attr("class", "img-fluid gif");
+                //Set the image/gif attr to load still
+                gifImage.attr("src", results[i].images.fixed_height_still.url);
+                gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+                gifImage.attr("data-animate", results[i].images.fixed_height.url);
+                gifImage.attr("data-state", "still");
 
-        //Looping through each 
-        for (var i = 0; i < results.length; i++) {
-            //Creating and storing a div tag
-            var gifDiv = $("<div>").attr("class", "gifDiv float-md-left");
-            //console.log();
-            //Creating and storing an image tag
-            var gifImage = $("<img>");
-            //Setting the attribute of the image
-            gifImage.attr("class", "img-fluid gif");
-            //Set the image/gif attr to load still
-            gifImage.attr("src", results[i].images.fixed_height_still.url);
-            gifImage.attr("data-still", results[i].images.fixed_height_still.url);
-            gifImage.attr("data-animate", results[i].images.fixed_height.url);
-            gifImage.attr("data-state", "still");
+                //property pulled off the result item's rating
+                var rating = $("<p>").text(" Rating: " + results[i].rating).attr("class", "rating");
 
-            //property pulled off the result item's rating
-            var rating = $("<p>").text(" Rating: " + results[i].rating).attr("class", "rating");
+                //Appending the image
+                gifDiv.append(gifImage);
+                //Appending the rating
+                gifDiv.append(rating);
 
-            //Appending the image
-            gifDiv.append(gifImage);
-            //Appending the rating
-            gifDiv.append(rating);
+                $(".gif-container").prepend(gifDiv);
 
-            $(".gif-container").prepend(gifDiv);
+            }
+        });
+    };
 
+    //Add event listener to all elements with class
+    $(document).on("click", ".gif", function () {
+        //Adding data-attribute
+        var state = $(this).attr("data-state");
+        //Add if/else statement to animate or still gif
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
         }
+    })
+
+
+    //Function for displaying athlete buttons
+    function makeButtons() {
+        $("#gif-buttons").empty();
+
+        //Loop through array of topics 
+        for (var i = 0; i < topics.length; i++) {
+            //Dynamically gernerate buttons for each athletes
+            var gifButton = $("<button>");
+            //Adding a attr to the buttons
+            // gifButton.attr("type", "button");
+            gifButton.attr("class", "btn btn-dark mr-2 mt-2 athletes");
+            gifButton.text(topics[i]);
+            gifButton.attr("data-athletes", topics[i]);
+            gifButton.attr("id", "gif-button");
+            //Append button to the div
+            $("#gif-buttons").append(gifButton);
+        }
+    }
+
+
+    //Create for when user searches new athlete
+    //Eventlistener to add gif
+    $("#add-gif").on("click", function (event) {
+        //Deleting the previous gifs
+        event.preventDefault();
+        //Grab input from textbox
+        var athletes = $("#gif-input").val().trim();
+        //Add input from textbox to array
+        topics.push(athletes);
+        makeButtons();
     });
-};
 
-//Add event listener to all elements with class
-$(document).on("click", ".gif", function () {
-    //Adding data-attribute
-    var state = $(this).attr("data-state");
-    //Add if/else statement to animate or still gif
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-})
-
-
-//Function for displaying athlete buttons
-function makeButtons() {
-    $("#gif-buttons").empty();
-
-    //Loop through array of topics 
-    for (var i = 0; i < topics.length; i++) {
-        //Dynamically gernerate buttons for each athletes
-        var gifButton = $("<button>");
-        //Adding a attr to the buttons
-        gifButton.attr("type", "button").attr("class", "btn btn-dark mr-2 mt-2 athletes").text(topics[i]).attr("data-athletes", topics[i]).attr("id", "gif-button");
-        //Append button to the div
-        $("#gif-buttons").append(gifButton);
-    }
-}
-
-
-//Create for when user searches new athlete
-//Eventlistener to add gif
-$("#add-gif").on("click", function (event) {
-    //Deleting the previous gifs
-    event.preventDefault();
-    //Grab input from textbox
-    var athletes = $("#gif-input").val().trim();
-    //Add input from textbox to array
-    topics.push(athletes);
+    //Calling document on click to diplay initially on page
+    $(document).on("click", ".athletes", displayAthletesGif);
+    //call makeButtons function to display buttons
     makeButtons();
-});
 
-//Calling document on click to diplay initially on page
-$(document).on("click", ".athletes", displayAthletesGif);
-//call makeButtons function to display buttons
-makeButtons();
+});
